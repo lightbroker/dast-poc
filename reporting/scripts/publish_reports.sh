cefe75b0481c6657c80007c38aef3b9e11e078ea
lightbroker <42450907+lightbroker@users.noreply.github.com>
2024-02-06 13:33:56 -0700

    can script be referenced?

---
+++ reporting/scripts/publish_reports.sh
@@ -0,0 +1,11 @@
+# TIMESTAMP=$(date +%s)
+# TARGET='altoro.testfire.net'
+# mkdir -p .zap/reports/
+# cat report_md.md > ".zap/reports/dast_$TARGET_$TIMESTAMP.md"
+# cat report_json.json | jq > ".zap/reports/dast_$TARGET_$TIMESTAMP.json"
+# rm report_*
+# git config user.name github-actions
+# git config user.email github-actions@github.com
+# git add .
+# git commit -m "report output: $TIMESTAMP"
+# git push
