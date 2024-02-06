f8deaddbb2591d62ffe1847a2a99ad18d1a3d3a7
lightbroker <42450907+lightbroker@users.noreply.github.com>
2024-02-06 14:53:14 -0700

    pass execution context to publish script

--- reporting/scripts/publish_reports.sh
+++ reporting/scripts/publish_reports.sh
@@ -3,13 +3,14 @@ TIMESTAMP=$(TZ=UTC date +%s)
 UTC_MONTH=$(TZ=UTC date +%m)
 UTC_YEAR=$(TZ=UTC date +%Y)
 TARGET=$1
-DIR_PATH="reporting/output/${UTC_YEAR}/${UTC_MONTH}" 
+DIR_PATH="reporting/output/${UTC_YEAR}/${UTC_MONTH}"
+EXECUTION_CONTEXT=$2
 
 # create timestamped report output files and clean up
 mkdir -p ${DIR_PATH}
-cat report_md.md > ${DIR_PATH}/dast_${TARGET}_${TIMESTAMP}.md
-cat report_json.json | jq > ${DIR_PATH}/dast_${TARGET}_${TIMESTAMP}.json
-rm report_*
+cat ${EXECUTION_CONTEXT}/report_md.md > ${DIR_PATH}/dast_${TARGET}_${TIMESTAMP}.md
+cat ${EXECUTION_CONTEXT}/report_json.json | jq > ${DIR_PATH}/dast_${TARGET}_${TIMESTAMP}.json
+rm ${EXECUTION_CONTEXT}/report_*
 
 # stage, commit, and push to repo
 git config user.name github-actions
