337fa64f6b8896eae6ce4a9b3d26ded614de357f
lightbroker <42450907+lightbroker@users.noreply.github.com>
2024-02-06 13:21:15 -0700

    more clean up; remove .NET projects

--- src/DastPocTarget/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-namespace DastPocTarget.Models
-{
-    public class AppUser
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Message { get; set; }
-    }
-}
